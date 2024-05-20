73df103db4322ecdfc76ad7ce33b8787b998a535
starktony6969 <169042254+starktony6969@users.noreply.github.com>
2024-05-20 12:04:20 +0545

    MANY change

--- admin/inc/add_candidates.php
+++ admin/inc/add_candidates.php
@@ -1,34 +1,31 @@
 <?php 
 error_reporting(0);
  
- if(isset($_GET['added']))
-    {
+if(isset($_GET['added'])) {
 ?>
 <div class="alert alert-success my-3" role="alert">
     Candidate has been added successfully.
 </div>
 <?php 
-    }else if(isset($_GET['largeFile'])) {
+} else if(isset($_GET['largeFile'])) {
 ?>
 <div class="alert alert-danger my-3" role="alert">
-    Candidate image is too large, please upload small file (you can upload any image upto 2mbs.).
+    Candidate image is too large, please upload a smaller file (you can upload any image up to 2MB).
 </div>
 <?php
-    }else if(isset($_GET['invalidFile']))
-    {
+} else if(isset($_GET['invalidFile'])) {
 ?>
 <div class="alert alert-danger my-3" role="alert">
-    Invalid image type (Only .jpg, .png files are allowed) .
+    Invalid image type (Only .jpg, .png files are allowed).
 </div>
 <?php
-    }else if(isset($_GET['failed']))
-    {
+} else if(isset($_GET['failed'])) {
 ?>
 <div class="alert alert-danger my-3" role="alert">
     Image uploading failed, please try again.
 </div>
 <?php
-    }
+}
 
 ?>
 
@@ -40,35 +37,30 @@ error_reporting(0);
                 <select class="form-control" name="election_topic" required>
                     <option value="">Select Election</option>
                     <?php
-              include 'config.php'; 
-
-        $queryElections = "SELECT * FROM elections WHERE status = 'active'";
-        $resultElections = mysqli_query($db, $queryElections) or die(mysqli_error($db));
-
-           if (mysqli_num_rows($resultElections) > 0) {
-           while ($election = mysqli_fetch_assoc($resultElections)) {
-
-            $election_topic = $election['election_topic'];
-            $election_name = $election['election_topic'];
-
-
-            ?>
+                    include 'config.php'; 
+                    $queryElections = "SELECT * FROM elections WHERE status = 'active'";
+                    $resultElections = mysqli_query($db, $queryElections) or die(mysqli_error($db));
+                    if (mysqli_num_rows($resultElections) > 0) {
+                        while ($election = mysqli_fetch_assoc($resultElections)) {
+                            $election_topic = $election['election_topic'];
+                            $election_name = $election['election_topic'];
+                    ?>
                     <option value="<?php echo ($election_topic); ?>"><?php echo($election_name); ?></option>
                     <?php
-        }
-    }
- else
-    ?>
+                        }
+                    } else {
+                    ?>
                     <option value="">Please add an election first</option>
+                    <?php
+                    }
+                    ?>
                 </select>
-
             </div>
-
             <div class="form-group">
                 <input type="text" name="candidate_name" placeholder="Candidate Name" class="form-control" required />
             </div>
             <div class="form-group">
-                <input type="text" name="Registration_No" placeholder="Registration_No" class="form-control" required />
+                <input type="text" name="Registration_No" placeholder="Registration No" class="form-control" required />
             </div>
             <div class="form-group">
                 <input type="file" name="candidate_photo" class="form-control" required />
@@ -80,67 +72,63 @@ error_reporting(0);
             <input type="submit" value="Add Candidate" name="addCandidateBtn" class="btn btn-success" />
         </form>
     </div>
+
     <?php 
-      $db = mysqli_connect("localhost", "root", "", "project") or die("Connectivity Failed");
+    $db = mysqli_connect("localhost", "root", "", "project") or die("Connectivity Failed");
 
-
-    
-
-    if(isset($_POST['addCandidateBtn']))
-    {
-
-
-        $candidate_name =$_POST['candidate_name'];
-        $Registration_No=$_POST['Registration_No'];
+    if(isset($_POST['addCandidateBtn'])) {
+        $candidate_name = $_POST['candidate_name'];
+        $Registration_No = $_POST['Registration_No'];
         $candidate_details = $_POST['candidate_details'];
-        $election_topic=$_POST['election_topic'];
+        $election_topic = $_POST['election_topic'];
         $inserted_on = date("Y-m-d");
-        $traggetted_folder="../assets/images/candidate_photos/"; 
-        $candidate_photo =$traggetted_folder .$_FILES['candidate_photo']['name'];
-        $candidate_photo_tmp_name = $_FILES['candidate_photo']['name'];
+        $target_folder = "../assets/images/candidate_photos/"; 
+        $candidate_photo = $target_folder . basename($_FILES['candidate_photo']['name']);
+        $candidate_photo_tmp_name = $_FILES['candidate_photo']['tmp_name'];
         $candidate_photo_type = strtolower(pathinfo($candidate_photo, PATHINFO_EXTENSION));
-        $allowed_types = array("jpg", "png", "jpeg", );
-        $image_size= $_FILES['candidate_photo']['size'];
+        $allowed_types = array("jpg", "png", "jpeg");
+        $image_size = $_FILES['candidate_photo']['size'];
+        $alreadyp = "notpresent";
 
+        if (!in_array($candidate_photo_type, $allowed_types)) {
+            header('Location: /project/admin/adminhome.php?addCandidatePage=1&invalidFile=1');
+            exit();
+        }
+        if ($image_size > 2000000) {
+            header('Location: /project/admin/adminhome.php?addCandidatePage=1&largeFile=1');
+            exit();
+        }
 
-            $query = "SELECT * FROM candidate_details";
+        $query = "SELECT * FROM candidate_details";
         $result = mysqli_query($db, $query);
         if (mysqli_num_rows($result) > 0) {
-        $sno = 1;
-        while ($row = mysqli_fetch_assoc($result)) 
-        {
-            // $alreadyp="notpresent";
-        //  echo $row['Registration_No'];
-          if($_POST['Registration_No'] == $row['Registration_No'])
-            {
-                // echo $row['Registration_No'];
-            $alreadyp = "present";
-            // echo $alreadyp;
+            while ($row = mysqli_fetch_assoc($result)) {
+                if ($Registration_No == $row['Registration_No']) {
+                    $alreadyp = "present";
+                }
             }
         }
-        if($alreadyp=="present")
-        {
-            echo "";
+
+        if ($alreadyp == "present") {
+            header('Location: /project/admin/adminhome.php?addCandidatePage=1&failed=1');
+            exit();
+        } else {
+            if (move_uploaded_file($candidate_photo_tmp_name, $candidate_photo)) {
+                $result = mysqli_query($db, "INSERT INTO candidate_details(candidate_name, Registration_No, candidate_photo, candidate_details, election_topic) VALUES('$candidate_name', '$Registration_No', '$candidate_photo', '$candidate_details', '$election_topic')") or die(mysqli_error($db));
+                if ($result > 0) {
+                    header('Location: /project/admin/adminhome.php?addCandidatePage=1&added=1');
+                    exit();
+                } else {
+                    header('Location: /project/admin/adminhome.php?addCandidatePage=1&failed=1');
+                    exit();
+                }
+            } else {
+                header('Location: /project/admin/adminhome.php?addCandidatePage=1&failed=1');
+                exit();
+            }
         }
-        else{
-            $result=mysqli_query($db,"INSERT INTO candidate_details(candidate_name, Registration_No, candidate_photo, candidate_details,election_topic) VALUES('". $candidate_name ."','". $Registration_No ."','". $candidate_photo ."', '". $candidate_details ."','".$election_topic."')") or die(mysqli_error($db));
-
-
-            // if($result>0)
-            // {
-            //    header('Location: /project/admin/adminhome.php?addCandidatePage=1'); 
-        
-            // }
-        
-        }
-}
-
-    
-  
-} 
-
-?>
-
+    }
+    ?>
 
     <div class="col-8">
         <h3>Candidate Details</h3>
@@ -150,44 +138,33 @@ error_reporting(0);
                     <th scope="col">S.No</th>
                     <th scope="col">Photo</th>
                     <th scope="col">Name</th>
-                    <th scope="col">Registration_No</th>
+                    <th scope="col">Registration No</th>
                     <th scope="col">Details</th>
                     <th scope="col">Election</th>
-                    <th scope="col">Action </th>
-
+                    <th scope="col">Action</th>
                 </tr>
             </thead>
             <tbody>
-
                 <?php
-            
-            $query = "SELECT * FROM candidate_details";
-            $result = mysqli_query($db, $query);
-            if (mysqli_num_rows($result) > 0) {
-            $sno = 1;
-            while ($row = mysqli_fetch_assoc($result)) 
-            {
-                $election_name = $row['election_topic'];
-                $candidate_photo = $row['candidate_photo'];
-                
-
-                    echo "<tr>";
-                    echo "<td>" . $sno++ . "</td>";
-
-                    echo "<td><img src='" . $candidate_photo ."'class=candidate_photo></td>";
-
-                    echo "<td>" . $row['candidate_name'] . "</td>";
-
-                    echo "<td>" . $row['Registration_No'] . "</td>";
-
-                    echo "<td>" . $row['candidate_details'] . "</td>";
-
-                    echo "<td>" . $row['election_topic'] . "</td>";
-
-                   
-                    
-            echo "<td><a href='/project/admin/inc/edit_candidate.php?id=" .$row['id']. "'>Edit</a> | <a name ='delcandidatebtn' href='/project/admin/inc/delete_candidate.php?id=" .$row['id']. "'>Delete</a></td>";
-            echo "</tr>";
+                $query = "SELECT * FROM candidate_details";
+                $result = mysqli_query($db, $query);
+                if (mysqli_num_rows($result) > 0) {
+                    $sno = 1;
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $candidate_photo = $row['candidate_photo'];
+                        echo "<tr>";
+                        echo "<td>" . $sno++ . "</td>";
+                        echo "<td><img src='" . $candidate_photo . "' class='candidate_photo' style='width: 50px; height: 50px;'></td>";
+                        echo "<td>" . $row['candidate_name'] . "</td>";
+                        echo "<td>" . $row['Registration_No'] . "</td>";
+                        echo "<td>" . $row['candidate_details'] . "</td>";
+                        echo "<td>" . $row['election_topic'] . "</td>";
+                        echo "<td><a href='/project/admin/inc/edit_candidate.php?id=" . $row['id'] . "'>Edit</a> | <a name='delcandidatebtn' href='/project/admin/inc/delete_candidate.php?id=" . $row['id'] . "'>Delete</a></td>";
+                        echo "</tr>";
+                    }
                 }
-            }
-            ?>
+                ?>
+            </tbody>
+        </table>
+    </div>
+</div>
--- admin/inc/add_elections.php
+++ admin/inc/add_elections.php
@@ -1,52 +1,98 @@
 <div class="row my-3">
-    <div class="col-4">
-        <h3>Add New Election</h3>
+    <div class="col-4 shadow-sm p-4">
+        <h4>Add New Election</h4>
+        <?php
+        // Validate form submission
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Validate Election Topic
+            if (empty($_POST["election_topic"])) {
+                $election_topic_err = "Election topic is required";
+            } else {
+                $election_topic = test_input($_POST["election_topic"]);
+            }
+
+            // Validate Number of Candidates
+            if (empty($_POST["number_of_candidates"])) {
+                $number_of_candidates_err = "Number of candidates is required";
+            } else {
+                $number_of_candidates = test_input($_POST["number_of_candidates"]);
+            }
+
+            // Validate Starting Date
+            if (empty($_POST["starting_date"])) {
+                $starting_date_err = "Starting date is required";
+            }
+
+            // Validate Ending Date
+            if (empty($_POST["ending_date"])) {
+                $ending_date_err = "Ending date is required";
+            }
+        }
+
+        // Function to sanitize input data
+        function test_input($data)
+        {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+        ?>
+
         <form method="POST">
             <div>
-                <input type="text" name="election_topic" placeholder="Election Topic" class="form-control"
-                    required /><br>
+                <label for="election_topic">Election Topic</label>
+                <input type="text" name="election_topic" id="election_topic" placeholder="Election Topic"
+                    class="form-control" />
+                <span class="text-danger"><?php echo isset($election_topic_err) ? $election_topic_err : ''; ?></span>
             </div>
             <div>
-                <input type="number" name="number_of_candidates" placeholder="No of Candidates" class="form-control"
-                    required /><br>
+                <label for="number_of_candidates">Number of Candidates</label>
+                <input type="number" name="number_of_candidates" id="number_of_candidates"
+                    placeholder="No of Candidates" class="form-control" />
+                <span
+                    class="text-danger"><?php echo isset($number_of_candidates_err) ? $number_of_candidates_err : ''; ?></span>
             </div>
             <div>
-                <input type="text" onfocus="this.type='Date'" name="starting_date" placeholder="Starting Date"
-                    class="form-control" required /><br>
+                <label for="starting_date">Starting Date</label>
+                <input type="date" name="starting_date" id="starting_date" class="form-control" />
+                <span class="text-danger"><?php echo isset($starting_date_err) ? $starting_date_err : ''; ?></span>
             </div>
             <div>
-                <input type="text" onfocus="this.type='Date'" name="ending_date" placeholder="Ending Date"
-                    class="form-control" required /><br>
+                <label for="ending_date">Ending Date</label>
+                <input type="date" name="ending_date" id="ending_date" class="form-control" />
+                <span class="text-danger"><?php echo isset($ending_date_err) ? $ending_date_err : ''; ?></span>
             </div>
             <input type="submit" value="Add Election" name="addElectionBtn" class="btn btn-success" /><br>
         </form>
+
     </div>
 
-    <?php 
+
+    <?php
     include 'config.php';
 
-    if(isset($_POST['addElectionBtn']))
-    {
-        $election_topic =$_POST['election_topic'];
-        $number_of_candidates =$_POST['number_of_candidates'];
+    if (isset($_POST['addElectionBtn'])) {
+        $election_topic = $_POST['election_topic'];
+        $number_of_candidates = $_POST['number_of_candidates'];
         $starting_date = $_POST['starting_date'];
         $ending_date = $_POST['ending_date'];
         $inserted_on = date("Y-m-d");
-    
+
         // Create date objects
         $date1 = date_create($inserted_on);
         $date2 = date_create($starting_date);
         $date3 = date_create($ending_date);
-    
+
         // Calculate difference
         $diff = date_diff($date1, $date2);
-    
+
         // Check if the starting date is in the past
         if ($starting_date < $inserted_on) {
-            echo "Error: Past date can't be selected/added.";
+            echo "Error: please fill the details.";
             exit;
         }
-    
+
         // Determine status
         if ($inserted_on < $starting_date) {
             $status = "Coming";
@@ -55,12 +101,11 @@
         } else {
             $status = "Expired";
         }
-    
 
-        
-        mysqli_query($db, "INSERT INTO elections(election_topic, no_of_candidates, starting_date, ending_date, status,  inserted_on) VALUES('". $election_topic ."', '". $number_of_candidates ."', '". $starting_date ."', '". $ending_date ."', '". $status ."', '". $inserted_on ."')") or die(mysqli_error($db));
-        
-     } ?>
+
+
+        mysqli_query($db, "INSERT INTO elections(election_topic, no_of_candidates, starting_date, ending_date, status,  inserted_on) VALUES('" . $election_topic . "', '" . $number_of_candidates . "', '" . $starting_date . "', '" . $ending_date . "', '" . $status . "', '" . $inserted_on . "')") or die(mysqli_error($db));
+    } ?>
 
     <div class="col-8">
         <h3>Upcoming Elections</h3>
@@ -79,24 +124,44 @@
             <tbody>
 
                 <?php
-            include'config.php';
-            $query = "SELECT * FROM elections";
-            $result = mysqli_query($db, $query);
-            if (mysqli_num_rows($result) > 0) {
-                $sno = 1;
-                while ($row = mysqli_fetch_assoc($result)) {
-                    echo  "<tr>";
-                    echo "<td>" . $sno++ . "</td>";
-                    echo "<td>" . $row['election_topic'] . "</td>";
-                    echo "<td>" . $row['no_of_candidates'] . "</td>";
-                    echo "<td>" . $row['starting_date'] . "</td>";
-                    echo "<td>" . $row['ending_date'] . "</td>";
-                    echo "<td>" . $row['status'] . "</td>";
-                    // Add your action buttons or links here
-                    echo "<td><a href='/project/admin/inc/edit_elections.php?id=" . $row['id'] . "'>Edit</a> | <a name ='delelectionsbtn' href='/project/admin/inc/delete_elections.php?id=" . $row['id'] . "'>Delete</a></td>";
-                    echo "</tr>";
+                include 'config.php';
+                $query = "SELECT * FROM elections";
+                $result = mysqli_query($db, $query);
+                if (mysqli_num_rows($result) > 0) {
+                    $sno = 1;
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo  "<tr>";
+                        echo "<td>" . $sno++ . "</td>";
+                        echo "<td>" . $row['election_topic'] . "</td>";
+                        echo "<td>" . $row['no_of_candidates'] . "</td>";
+                        echo "<td>" . $row['starting_date'] . "</td>";
+                        echo "<td>" . $row['ending_date'] . "</td>";
+                        echo "<td>" . $row['status'] . "</td>";
+                        // Add your action buttons or links here
+                        echo "<td><a href='/project/admin/inc/edit_elections.php?id=" . $row['id'] . "'>Edit</a> | ";
+                        echo "<a href='/project/admin/inc/delete_elections.php?id=" . $row['id'] . "' onclick='return confirmDelete()'>Delete</a></td>";
+                        echo "</tr>";
+                    }
+                } else {
+                    echo "<tr><td colspan='7'>No upcoming elections found.</td></tr>";
                 }
-            } else {
-                echo "<tr><td colspan='7'>No upcoming elections found.</td></tr>";
-            }
-            ?>
+                ?>
+
+                <style>
+                label {
+                    font-weight: 500;
+                }
+                </style>
+
+                <script>
+                // Get current date
+                var today = new Date().toISOString().split('T')[0];
+                // Set minimum date for starting_date input
+                document.getElementById('starting_date').min = today;
+                // Set minimum date for ending_date input
+                document.getElementById('ending_date').min = today;
+
+                function confirmDelete() {
+                    return confirm('Are you sure you want to delete?');
+                }
+                </script>
