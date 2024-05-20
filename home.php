d2ea90395d4e17e2457d437c73345696582b9a85
starktony6969 <169042254+starktony6969@users.noreply.github.com>
2024-05-20 21:10:39 +0545

    MANY change

--- home.php
+++ home.php
@@ -8,100 +8,43 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
-
+    <link rel="stylesheet" href="style/home.css">
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-success">
-        <div class="container-fluid">
-            <div class="d-flex justify-content-between w-100">
-
-                <div class="col d-flex justify-content-center">
-                    <a class="navbar-brand " href="home.php">
-                        <h3>Online Voting System </h3>
-                    </a>
-                </div>
-
-            </div>
 
 
+    <section>
+        <div class="content">
+            <h1>Online Voting System</h1>
+            <p>
+                Welcome to our Online Voting Platform, the premier destination for secure, transparent, and
+                accessible online voting. Our platform offers a seamless and straightforward voting process for a
+                wide range of elections, from local community polls to organizational elections. With cutting-edge
+                security and user-friendly design, participating in democracy has never been easier.
+            </p>
+            <button><a href="index.php" style="text-decoration: none;">Get Started</a></button>
         </div>
-    </nav>
 
+        <div class="swiper ">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="img/pht.jpg" />
 
-    <div id="carouselExampleCaptions" class="carousel slide carousel-fade">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
-                aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-                aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="img/fontview.jpg" width="1400" height="686" class="d-block w-100" alt="..." />
-                <div class="carousel-caption d-none d-md-block">
-                    <h5 style="color: black;">We are here</h5>
-                    <p style="color: black;">
-                        To provide you this website development for Online Voting System,Welcome to our Online Voting
-                        Platform, the premier destination for secure, transparent, and accessible
-                        online voting. Our platform offers a seamless and straightforward voting process for a wide
-                        range of
-                        elections, from local community polls to organizational elections. With cutting-edge security
-                        and
-                        user-friendly design, participating in democracy has never been easier.
-                    </p>
-                    <div class="mx-2">
-                        <a href="index.php"> <button type="button" class="btn btn-danger">Login</button></a>
-                        <a href="register.php"> <button type="button" class="btn btn-danger">Sign Up</button></a>
+                    <div class="overlay">
+                        <h1>Secure and Easy</h1>
+                        <p>
+                            Now you can vote Us, from wherever you are at anytime anywhere.
+                        </p>
+
                     </div>
-
-
-
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="img/phh.jpg" width="1400" height="686" class="d-block w-100" alt="..." />
-                <div class="carousel-caption d-none d-md-block">
-                    <h5 style="color: white;">Acknowledge</h5>
-                    <p style="color: white;">You can vote from anywhere you are, and there is less chance of any error .
-                    </p>
-                    <div class="mx-2">
-                        <a href="index.php"> <button type="button" class="btn btn-primary">Login</button></a>
-                        <a href="register.php"> <button type="button" class="btn btn-primary">Sign Up</button></a>
-                    </div>
-
-
-
                 </div>
 
 
-            </div>
-            <div class="carousel-item">
-                <img src="img/BackTalk.jpg" width="1400" height="686" class="d-block w-100" alt="..." />
-                <div class="carousel-caption d-none d-md-block">
-                    <h5 style="color:black ;">Concept</h5>
-                    <p style="color: black;">We begin by understanding your vision and goals.</p>
-                    <div class="mx-2">
-                        <a href="index.php"> <button type="button" class="btn btn-success">Login</button></a>
-                        <a href="register.php"> <button type="button" class="btn btn-success">Sign Up</button></a>
-                    </div>
-
-                </div>
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
+    </section>
+
 
 
 
@@ -109,6 +52,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
+    \
 </body>
 
 </html>
--- voters/home.php
+++ voters/home.php
@@ -1,5 +1,6 @@
-
-<head><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script></head>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</head>
 <?php
 session_start(); // Ensure session is started
 require_once("inc/header.php");
@@ -18,11 +19,11 @@ if (!isset($_SESSION['user_id'])) {
  
 ?>
 
-    <div class="row my-3">
-        <div class="col-12">
-            <h3> Voters Panel </h3>
+<div class="row my-3">
+    <div class="col-12">
+        <h3> Voters Panel </h3>
 
-            <?php
+        <?php
 
         $queryElections = "SELECT * FROM elections WHERE status = 'active'";
         $resultElections = mysqli_query($db, $queryElections) or die(mysqli_error($db));
@@ -35,22 +36,24 @@ if (!isset($_SESSION['user_id'])) {
           
 
              }  ?>
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th colspan="4" class="bg-green text-white"><h5>  ELECTION TOPIC: <?php echo strtoupper($election_name); ?></h5></th>
-                                </tr>
-                                <tr>
-                                    <th> Photo </th>
-                                    <th> Registration_No </th>
-                                    <th> Candidate Details </th>
-                                    <th> No of Votes </th>
-                                    <th> Action </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                
-                            <?php
+        <table class="table">
+            <thead>
+                <tr>
+                    <th colspan="4" class="bg-green text-white">
+                        <h5> ELECTION TOPIC: <?php echo strtoupper($election_name); ?></h5>
+                    </th>
+                </tr>
+                <tr>
+                    <th> Photo </th>
+                    <th> Registration_No </th>
+                    <th> Candidate Details </th>
+                    <th> No of Votes </th>
+                    <th> Action </th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php
                             
                                  $query= ("SELECT * FROM candidate_details WHERE election_topic= '". $election_name ."'") ;
                                  $result = mysqli_query($db, $query) or die(mysqli_error($db)) ;
@@ -65,14 +68,14 @@ if (!isset($_SESSION['user_id'])) {
                                     $fetchingVotes = mysqli_query($db, "SELECT * FROM votings WHERE candidate_id = '". $candidate_id . "'") or die(mysqli_error($db));
                                     $totalVotes = mysqli_num_rows($fetchingVotes);
                             ?>
-                                    <tr>
-                                        <td> <img src="<?php echo $candidate_photo; ?>" class="candidate_photo"> </td>
+                <tr>
+                    <td> <img src="<?php echo $candidate_photo; ?>" class="candidate_photo"> </td>
 
-                                        <td><?php echo $Registration_No?></td>
-                                        <td><?php echo "<b>" . $row['candidate_name'] . "</b><br />" . $row['candidate_details']; ?></td>
-                                        <td><?php echo $totalVotes; ?></td>
-                                        <td>
-                                    <?php
+                    <td><?php echo $Registration_No?></td>
+                    <td><?php echo "<b>" . $row['candidate_name'] . "</b><br />" . $row['candidate_details']; ?></td>
+                    <td><?php echo $totalVotes; ?></td>
+                    <td>
+                        <?php
                                             $checkIfVoteCasted = mysqli_query($db, "SELECT * FROM votings WHERE voters_id = '". $_SESSION['user_id'] ."' AND election_id = '". $election_id ."'") or die(mysqli_error($db));    
                                             $isVoteCasted = mysqli_num_rows($checkIfVoteCasted);
 
@@ -85,28 +88,30 @@ if (!isset($_SESSION['user_id'])) {
                                                 {
                                     ?>
 
-                                                    <img src="../assets/images/vote.png" width="100px;">
-                                    <?php
+                        <img src="../assets/images/vote.png" width="100px;">
+                        <?php
                                                 }
                                             }else {
                                     ?>
-                                                <button class="btn btn-md btn-success" onclick="CastVote(<?php echo $election_id; ?>, <?php echo $candidate_id; ?>, <?php echo $_SESSION['user_id']; ?>)"> Vote </button>
-                                    <?php
+                        <button class="btn btn-md btn-success"
+                            onclick="CastVote(<?php echo $election_id; ?>, <?php echo $candidate_id; ?>, <?php echo $_SESSION['user_id']; ?>)">
+                            Vote </button>
+                        <?php
                                             }
 
                                             
                                     ?>
 
 
-                                    </td>
-                                    </tr>
-                            <?php
+                    </td>
+                </tr>
+                <?php
                                 }
                             ?>
-                            </tbody>
+            </tbody>
 
-                        </table>
-                <?php
+        </table>
+        <?php
                     
                     }
                 }else {
@@ -114,30 +119,27 @@ if (!isset($_SESSION['user_id'])) {
                 }
             ?>
 
-            
-        </div>
+
     </div>
+</div>
 
 
-    <script>
-    const CastVote = (election_id, customer_id, voters_id) => 
-    {
-        $.ajax({
-            type: "POST", 
-            url: "inc/ajaxCalls.php",
-            data: "e_id=" + election_id + "&c_id=" + customer_id + "&v_id=" + voters_id, 
-            success: function(response) {
-                
-                if(response == "Success")
-                {
-                    location.assign("home.php?voteCasted=1");
-                }else {
-                    location.assign("home.php?voteNotCasted=1");
-                }
+<script>
+const CastVote = (election_id, customer_id, voters_id) => {
+    $.ajax({
+        type: "POST",
+        url: "inc/ajaxCalls.php",
+        data: "e_id=" + election_id + "&c_id=" + customer_id + "&v_id=" + voters_id,
+        success: function(response) {
+
+            if (response == "Success") {
+                location.assign("home.php?voteCasted=1");
+            } else {
+                location.assign("home.php?voteNotCasted=1");
             }
-        });
-    }
-
+        }
+    });
+}
 </script>
 
 
